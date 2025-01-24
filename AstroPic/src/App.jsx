aac59a24d7545902d6318b90760b77d413a8d2de
Eric Van Hezewijk <ericvanhezewijk@uvic.ca>
2025-01-24 14:29:37 -0800

    Implemented dynamic sidebar and fixed page layout

--- AstroPic/src/App.jsx
+++ AstroPic/src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Main from './components/Main'
-import Footer from './components/Footer'
-import Sidebar from './components/Sidebar'
-import { useState } from 'react'
+import React from "react";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import { useState, useEffect } from "react";
 
 function App() {
-
   const [showModal, setShowModal] = useState(false);
 
   function handleToggleModal() {
@@ -13,15 +12,28 @@ function App() {
     setShowModal(!showModal);
   }
 
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log("DATA\n", data);
+      } catch {
+        console.log(err.message);
+      }
+    }
+  }, []);
+
   return (
     <>
       <Main />
-        {showModal && (
-          <Sidebar handleToggleModal={handleToggleModal}/>
-        )}
-      <Footer handleToggleModal={handleToggleModal}/>
+      {showModal && <Sidebar handleToggleModal={handleToggleModal} />}
+      <Footer handleToggleModal={handleToggleModal} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
